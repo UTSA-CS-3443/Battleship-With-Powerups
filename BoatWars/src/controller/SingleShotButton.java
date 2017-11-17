54c683a023c905812ca54576a8ed294eedcde7a3
Jasmine Ramirez <j.ramirez.14th@gmail.com>
2017-11-17 01:17:56 -0600

    CSS and start screen

--- BoatWars/src/controller/SingleShotButton.java
+++ BoatWars/src/controller/SingleShotButton.java
@@ -1,11 +1,14 @@
 package controller;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import view.Board;
 import view.Cell;
 
-public class SingleShotButton {
+public class SingleShotButton{
 	private Button button = new Button();
 	public static void singleShot() {
-		Cell c = (Cell)event.getSource();
+
 	}
+	
 }
